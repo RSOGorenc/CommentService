b30905405ce2ce2ff93cfd255ccfa6a3ec8fc097
Aljaz <ar1961@student.uni-lj.si>
2020-01-05 17:04:29 +0100

    Added comments GET and POST.

---
+++ src/main/java/com/amproductions/commentsmicroservice/CommentEntry.java
@@ -0,0 +1,29 @@
+package com.amproductions.commentsmicroservice;
+
+import javax.json.bind.annotation.JsonbCreator;
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.json.bind.annotation.JsonbProperty;
+import java.time.LocalDate;
+import java.util.List;
+
+public class CommentEntry {
+    private String _id;
+    private String comment;
+
+    @JsonbCreator
+    public CommentEntry(@JsonbProperty("_id")String _id,
+                        @JsonbProperty("comment")String comment){
+        this._id = _id;
+        this.comment = comment;
+    }
+
+    public String get_id(){
+        return _id;
+    }
+
+    public String getComment(){
+        return comment;
+    }
+
+}
+
