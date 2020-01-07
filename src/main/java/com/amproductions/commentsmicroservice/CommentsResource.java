673b59c8d60931059a019d1bb445bb31c3430c16
Aljaz <ar1961@student.uni-lj.si>
2020-01-07 19:26:48 +0100

    aaa

--- src/main/java/com/amproductions/commentsmicroservice/CommentsResource.java
+++ src/main/java/com/amproductions/commentsmicroservice/CommentsResource.java
@@ -1,37 +1,42 @@
 package com.amproductions.commentsmicroservice;
 
-import com.kumuluz.ee.metrics.producers.MetricRegistryProducer;
-import org.eclipse.microprofile.metrics.Counter;
-import org.eclipse.microprofile.metrics.MetricRegistry;
+
+import com.kumuluz.ee.logs.cdi.Log;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 
-import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
-
-
+@Log
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Path("comments")
 public class CommentsResource {
 
+    private Logger log = Logger.getLogger(CommentsResource.class.getName());
+
     @GET
     @Path("/{objectID}")
     public Response getComments(@PathParam("objectID") String objectID) {
         try {
+            log.info("Calling getComments with objectID: " + objectID);
+
             Object comments = Database.GetComments(objectID).get("comments");
 
             if (comments == null) {
+                log.info("Return response status NOT FOUND on objectID: " + objectID);
                 return Response.status(Response.Status.NOT_FOUND).build();
             }
+            log.info("Return response status OK on objectID: " + objectID);
             return Response.status(Response.Status.OK).entity(comments).build();
 
         }
         catch (Exception e){
             e.printStackTrace();
         }
+        log.info("Internal server error");
         return  Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
     }
 
